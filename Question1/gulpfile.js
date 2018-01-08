0b335c574dc1c74e6ada59cfaf006d9f05d1b9c9
Lars <hjerrildlars@gmail.com>
2018-01-08 14:20:39 +0100

    changes

---
+++ Question1/gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding BeforeBuild='default, sass' />
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('default',
+    function() {
+        console.log("hello world");
+    });
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+
+paths.scss = paths.webroot + "scss/**/*.scss";
+paths.css = paths.webroot + "css/";
+
+gulp.task('sass',
+    function() {
+        gulp.src(paths.scss)
+            .pipe(sass())
+            .pipe(gulp.dest(paths.css));
+    });
+
+
+
